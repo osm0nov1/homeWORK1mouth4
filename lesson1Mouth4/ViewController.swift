09621c15197d258245d4698b118916305596eb70
osm0nov1 <osmonovrashit@gmail.com>
2022-02-09 19:08:27 +0600

    Initial Commit

---
+++ lesson1Mouth4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1Mouth4
+//
+//  Created by Rashit Osmonov on 9/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
